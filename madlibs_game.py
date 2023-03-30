c2bb22fb3ea7b1553210c1a1a37f6e1d3756e27e
Chick <chetwynd.chick@gmail.com>
2023-03-30 13:44:24 -0700

    uploading madlibs python script

---
+++ madlibs_game.py
@@ -0,0 +1,10 @@
+noun1 = input("Enter a plural noun: ")
+verb1 = input("Enter a doing word: ")
+noun2 = input("Enter a noun: ")
+verb2 = input("Enter a dong word: ")
+noun3 = input("Enter a noun: ")
+
+print(
+    "Jobie is a good boy, he likes " + noun1 + ". Sometimes, Jobie can be naughty by " + verb1 + " all of the " + noun2)
+
+print("But Jobie redeems himself by " + verb2 + " on top of the " + noun3 + " and it makes it all better")
